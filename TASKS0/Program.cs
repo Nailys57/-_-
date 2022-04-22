69e6bfae5885bd8788391f7e555da853f4784587
nailys57 <nik.rakovich@inbox.ru>
2022-04-22 14:28:05 +0300

    Домашние задания

--- TASKS0/Program.cs
+++ TASKS0/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Сравнение двух чисел
+Console.Write("Введите число 1: ");
+int a = Convert.ToInt32(Console.ReadLine() );
+
+Console.Write("Введите число 2: ");
+int b = Convert.ToInt32(Console.ReadLine() );
+
+if(a > b)
+Console.WriteLine($"{a} > {b}");
+else if(a < b)
+Console.WriteLine($"{a} < {b}");
+else
+Console.WriteLine($"{a} = {b}");
