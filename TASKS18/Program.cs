8efebeec8d5e6a09a5a4e8986162b60e60891bf8
nailys57 <nik.rakovich@inbox.ru>
2022-04-22 14:14:38 +0300

    dz

---
+++ TASKS18/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using static System.Console;
+Clear();
+
+Write("Введите число: ");
+int x=Int32.Parse(ReadLine());
+int stepen=3;
+
+for (int i = 1; i <= x; i++)
+{
+    WriteLine(Math.Pow(i, stepen));
+}  
