740d9f4eea51d274b5a366a0fa326cd0dc0c3245
nailys57 <nik.rakovich@inbox.ru>
2022-04-26 13:26:22 +0300

    Dz4

--- TASKS12/Program.cs
+++ TASKS12/Program.cs
@@ -5,10 +5,11 @@ Clear();
 
 Write("Введите количество элементов массива: ");
 int n = int.Parse(ReadLine());
+Random rnd = new Random();
  int[] a = new int[n];
             for (int i = 0; i < n; i++)
             {
-                a[i] = int.Parse(ReadLine());
+                a[i] = rnd.Next(1, 100);
                 
             }
             foreach (int s in a) Write(s+ " ");
