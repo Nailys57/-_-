0f969b9f51ad7e3df36fd8f23a8d156c9c7ff356
nailys57 <nik.rakovich@inbox.ru>
2022-05-18 14:03:12 +0300

    Double massive

---
+++ TASKS17/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using static System.Console;
+Clear();
+
+int[,] mass = {
+    {1, 4, 7, 2},
+    {5, 9, 2, 3},
+    {8, 4, 2, 4},
+};
+
+Write("введите номер элемента: ");
+int position = int.Parse(ReadLine());
+int array = mass.Length;
+
+if (position <= array)
+{
+    int i = 0;
+    int j = 0;
+    for (int counter = 2; counter <= position; counter++)
+    {
+        if(j < mass.GetLength(1) - 1)
+        {
+           j++;
+        }
+        else
+        {
+            j = 0;
+            i++;
+        }   
+    }
+    WriteLine(mass[i, j]);
+}
+else WriteLine("Такрго элемента нет");
--- Лекции_1/Program.cs
+++ Лекции_1/Program.cs
@@ -1,3 +1,19 @@
-﻿var rand = new Random().Next(-10, 10);
-Console.Write("{0:N1}", rand);
-Console.WriteLine();
+﻿        int[,] mass = new int[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+
+        Console.WriteLine("Введите искомое число");
+        int number = int.Parse(Console.ReadLine());
+
+
+
+        var index = (
+            from i in Enumerable.Range(0, mass.GetLength(0))
+            from j in Enumerable.Range(0, mass.GetLength(1))
+            where mass[i, j] == number
+            select  (i, j)
+            ).FirstOrDefault();
+
+
+
+        Console.WriteLine($"Индексы {index.i} {index.j}");
+
+        Console.ReadLine();
